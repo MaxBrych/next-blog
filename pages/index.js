d5b9fba701a60fff2b725ed86d213aaaefb88a5b
MaxBrych <max.brych03@gmail.com>
2022-09-02 07:41:57 +0200

    vercel deploy

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,8 @@
 import { useRouter } from "next/router";
 import imageUrlBuilder from "@sanity/image-url";
 import { useState, useEffect } from "react";
-import Link from "next/Link";
+import Link from "next/link";
+import Image from "next/image";
 import Blogpost from "../components/Blogpost";
 
 export default function Home({ posts }) {
@@ -30,7 +31,7 @@ export default function Home({ posts }) {
 
   console.log(mappedPosts);
   return (
-    <div className="relative bg-gray-500 shadow-[0px_4px_100px_rgba(255,_255,_255,_0.14)] [border:1px_solid_#fff] box-border w-full h-screen flex flex-col p-[0px_64px] items-center justify-center md:pl-[24px] md:pr-[2px] md:box-border sm:pl-[16px] sm:pr-[16px] sm:box-border">
+    <div className="relative bg-gray-500 shadow-[0px_4px_100px_rgba(255,_255,_255,_0.14)] [border:1px_solid_#fff] box-border w-full h-full flex flex-col p-[0px_64px] items-center justify-center md:pl-[24px] md:pr-[2px] md:box-border sm:pl-[16px] sm:pr-[16px] sm:box-border">
       <section className="rounded-[12px] w-[520px] flex flex-col p-[12px_0px] box-border items-start justify-center gap-[16px] text-left text-xl text-white font-roc-grotesk sm:w-full sm:rounded-[12px] sm:pl-[16px] sm:pr-[16px] sm:box-border">
         <div className="self-stretch flex flex-col items-start justify-start gap-[4px] md:w-full sm:w-full">
           <h1 className="m-[0] relative text-[inherit] tracking-[-0.4px] font-medium font-inherit inline-block sm:text-[32px]">
@@ -46,15 +47,16 @@ export default function Home({ posts }) {
             className="[text-decoration:none] rounded-[90px] bg-gray-300 w-[36px] h-[36px] shrink-0 flex flex-row p-[8px] box-border items-center justify-center"
             href="https://github.com/MaxBrych"
             target="_blank"
+            rel="noreferrer"
           >
-            <img
+            <Image
               className="relative w-[18px] h-[18px] shrink-0 overflow-hidden"
               alt=""
               src="./assets/akariconsgithubfill.svg"
             />
           </a>
           <a className="[text-decoration:none] rounded-[90px] bg-gray-300 w-[36px] h-[36px] shrink-0 flex flex-row p-[8px] box-border items-center justify-center">
-            <img
+            <Image
               className="relative w-[18px] h-[18px] shrink-0 overflow-hidden"
               alt=""
               src="./assets/akariconstwitterfill.svg"
